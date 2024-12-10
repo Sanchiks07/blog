d25110d4b047b22086c0290d152421745ef86731
ipb23.s.mitniece <ipb23.s.mitniece@pjvt.local>
2024-12-10 13:46:25 +0200

    izvadīju datus

--- index.php
+++ index.php
@@ -1,9 +1,18 @@
 <?php
 
-echo "heloings!";
+echo    "<style>
+            body {
+                background-color: #FC8EAC;
+                color: white;
+                font-size: 20px;
+            }
+        </style>";
+
+echo "heloings! <br><br>";
 
 // 1. Izveidot datu bāzi ar tabulu ✔
-// Savienot PHP ar datu bāzi
+// 2. Savienot PHP ar datu bāzi ✔ (DSN, PDO)
+// 3. Ivadīt datus  (statement)
 
 // db, nosaukums, parole, lietotājvārds
 // mysql_connect ❌
@@ -14,6 +23,15 @@ $dsn = "mysql:host=localhost;port=3306;user=root;password=;dbname=blog_ipb23;cha
 // PDO - PHP Data Object
 $pdo = new PDO($dsn);
 
+// 1. Sagatavot vaicājumu (statement)
+$statement = $pdo->prepare("SELECT * FROM posts");
+// 2. Izpildīt statement
+$statement->execute();
+// 3. Dabūt rezultātus
+$posts = $statement->fetchAll();
 
+echo "<pre>";
+var_dump($posts);
+echo "</pre>";
 
 ?>
