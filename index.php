02ac5747c72738e1e5020d669157e822fc164569
ipb23.s.mitniece <ipb23.s.mitniece@pjvt.local>
2025-01-06 11:29:02 +0200

    izvada izfiltrētos ierakstus

--- index.php
+++ index.php
@@ -6,27 +6,32 @@ require "Database.php";
 $config = require ("config.php");
 
 // Mērķis: Uztaisīt filtru - ierakstu meklēšana (06.01.25.)
-// 1. Izveidot moklēšnas joslu: HTML forma, kurā ir inout un submit poga
-// 2. Kaut kā sarakst;it PHP
-// 3. Atgriezt datus no SQL datu bāzes
+// 1. Izveidot moklēšnas joslu: HTML forma, kurā ir input un submit poga ✅
+// 2. Kaut kā sarakstīt PHP ✅
+// 3. Atgriezt datus no SQL datu bāzes ✅
 
 echo "<link rel='stylesheet' href='blog.css'>";
 
 // 1. Izveidot datu bāzi ar tabulu ✅
 // 2. Savienot PHP ar datu bāzi ✅ (DSN, PDO) (Database.php)
 // 3. Ivadīt datus uz HTML ✅ (statement, dumb and die "dd" + PDO) (Database.php)
+
 // fetchAll(PDO::FETCH_ASSOC) - Dabūt rezultātus kā asociatīvo masīvu, const = 2
 
 // db, nosaukums, parole, lietotājvārds
 // mysql_connect ❌
 
 $db = new Database($config["database"]);
-$posts = $db->query("SELECT * FROM posts")->fetchAll(PDO::FETCH_ASSOC);
+$posts = $db->query("SELECT * FROM posts;")->fetchAll();
+
+if (isset($_GET["search_query"]) && $_GET["search_query"] !="") {
+    $posts = $db->query("SELECT * FROM posts WHERE content LIKE '%" . $_GET["search_query"] . "%';")->fetchAll();
+};
 
 echo "<h1>Blogs</h1>";
 
 echo "<form>";
-echo "<input />";
+echo "<input name='search_query' />";
 echo "<button>Meklēt</button>";
 echo "</form>";
 
