e8f5ba4467fc6728aa9b4918312907a3cb46157a
ipb23.s.mitniece <ipb23.s.mitniece@pjvt.local>
2025-01-07 15:01:09 +0200

    categoriess un css

--- categories.php
+++ categories.php
@@ -1,6 +1,6 @@
 <?php
 
-echo "<link rel='stylesheet' href='blog.css'>";
+echo "<link rel='stylesheet' href='categories.css'>";
 require "Database.php";
 
 $config = require ("config.php");
@@ -8,13 +8,14 @@ $db = new Database($config["database"]);
 
 $select = "SELECT * FROM categories";
 $params = [];
-if (isset($_GET["search_query"]) && $_GET["search_query"] !="") {
+if (isset($_GET["search_query"]) && $_GET["search_query"] != "") {
     $search_query = "%" . $_GET["search_query"] . "%";
     $select .= " WHERE category_name LIKE :kategorija;";
     $params = ["kategorija" => $search_query];
 }
 $categories = $db->query($select, $params)->fetchAll();
 
+echo "<div class='container'>"; // atver container div
 echo "<h1>Kategorijas</h1>";
 
 echo "<form>";
@@ -29,7 +30,8 @@ if (count($categories) == 0) {
 echo "<ul>";
 foreach ($categories as $category) {
     echo "<li>" . $category["category_name"] . "</li>";
-};
+}
 echo "</ul>";
+echo "</div>"; // aizver container div
 
 ?>
