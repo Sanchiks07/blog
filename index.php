d6d1d088bcce329240437e6ab0a6b518ae033366
ipb23.s.mitniece <ipb23.s.mitniece@pjvt.local>
2025-01-09 11:28:40 +0200

    maršrutētājs, nepabeigts un ar kļūdām

--- index.php
+++ index.php
@@ -2,34 +2,9 @@
 
 require "functions.php";
 require "Database.php";
+require "router.php";
 
 $config = require ("config.php");
-
-// 1. Izveidot datu bāzi ar tabulu ✅
-// 2. Savienot PHP ar datu bāzi ✅ (DSN, PDO) (Database.php)
-// 3. Ivadīt datus uz HTML ✅ (statement, dumb and die "dd" + PDO) (Database.php)
-
-// fetchAll(PDO::FETCH_ASSOC) - Dabūt rezultātus kā asociatīvo masīvu, const = 2
-// " \"nam " - \ nozīmē "escape", lai nākamās pēdiņas neņem kā nobeigumu priekš simbolu virknes
-
-// Mērķis: Uztaisīt filtru - ierakstu meklēšana (06.01.25.)
-// 1. Izveidot moklēšnas joslu: HTML forma, kurā ir input un submit poga ✅
-// 2. Kaut kā sarakstīt PHP ✅
-// 3. Atgriezt datus no SQL datu bāzes ✅
-
 $db = new Database($config["database"]);
-
-$select = "SELECT * FROM posts";
-$params = [];
-if (isset($_GET["search_query"]) && $_GET["search_query"] !="") {
-    $search_query = "%" . $_GET["search_query"] . "%";
-    $select .= " WHERE content LIKE :nosaukums;"; // Sagatavotais vaicājums
-    $params = ["nosaukums" => $search_query]; // Saistītais parametrs
-}
-$posts = $db->query($select, $params)->fetchAll();
-
-$pageTitle = "Blogs";
-$style = "css/style.css";
-require "views/index.view.php"; // vienmēr atrodas faila pēdējā rindā
 
 ?>
