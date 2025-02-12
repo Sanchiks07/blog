e43eaa1529601093118c56dd3a807afb182fe0b8
ipb23.s.mitniece <ipb23.s.mitniece@pjvt.local>
2025-02-12 12:27:10 +0200

    salaboju delete.php

--- controllers/posts/delete.php
+++ controllers/posts/delete.php
@@ -1,21 +1,15 @@
 <?php
 
-require "Validator.php";
-
 if (!isset($_POST["id"])) {
-    redirectIfNotFound();
-}
+    $params = ["id" => $_POST["id"]];
+    $sql = "DELETE FROM posts WHERE id = :id;";
+    $post = $db->query($sql, $params)->fetch();
 
-if (!Validator::string($_POST["id"])) {
-        $errors["content"] = "Dzēšana nav iespējama";
-    }
-    else if (empty($errors)) {
-        $params = ["id" => $_POST["id"]];
-        $sql = "DELETE FROM posts WHERE id = :id;";
-        $post = $db->query($sql, $params)->fetch();
-        
-        header("Location: /");
-        exit();
+    header("Location: /");
+    exit();
+}
+else {
+    redirectIfNotFound();
 }
 
 ?>
